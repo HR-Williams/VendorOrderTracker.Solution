ccfc10869806541756364ea1b299ecd3fa45246c
HR Williams <halrubinwilliams@gmail.com>
2021-05-10 16:57:21 -0700

    Add passing test for Vendor Constructor and start of vendor class

---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    // public string Name { get; set; }
+    // // public int Id { get; }
+    // public string Description { get; set; }
+  }
+}
