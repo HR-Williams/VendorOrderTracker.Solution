c56ef53215be6e1d5af2837f4cae99b1b0cb3958
HR Williams <halrubinwilliams@gmail.com>
2021-05-23 02:10:36 -0700

    Update Order model and tests for id

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,14 @@ using System;
 namespace VendorOrderTracker.Tests
 {
   [TestClass] 
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderCustructor_CreatesInstanceOfOrder_Order()
     {
@@ -68,6 +74,47 @@ namespace VendorOrderTracker.Tests
       Order newOrder= new Order(title, description, price, date);
       DateTime result = newOrder.Date;
       Assert.AreEqual(date, result);
+    }
+
+    [TestMethod]
+    public void SetTitle_SetTitle_String()
+    {
+      string title = "CRI Regular";
+      string description =
+      "10 pastries";
+      int price = 17;
+      DateTime date = DateTime.Today;
+      Order newOrder = new Order(title, description, price, date);
+      string updatedTitle = "CRI Standing Order";
+      newOrder.Title= updatedTitle;
+      string result = newOrder.Title;
+      Assert.AreEqual(updatedTitle, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      string title01 = "CRI Regular";
+      string title02 = "Standing Order";
+      string description01 = "10 pastries";
+      string description02 = "8 pastries";
+      int price01 = 17;
+      int price02 = 14;
+      DateTime date01 = DateTime.Today;
+      DateTime date02 = DateTime.Today;
+      Order newOrder1 = new Order(title01, description01, price01, date01);
+      Order newOrder2 = new Order(title02, description02, price02, date02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result); 
     }
   }
 }
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,19 +5,35 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
-    public string Title { get; }
-    
-    public string Description { get; }
-
+    public string Title { get; set; }
+    public string Description { get; set; }
     public int Price { get; }
-
     public DateTime Date { get; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+
     public Order(string title, string description, int price, DateTime date)
     {
       Title = title;
       Description = description;
       Price = price;
       Date = DateTime.Today;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
