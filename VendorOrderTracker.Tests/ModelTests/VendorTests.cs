7aee89e6dfedda8cfb528c7449aeea7c8b8a03f7
HR Williams <halrubinwilliams@gmail.com>
2021-05-23 02:11:34 -0700

    Pass test to associate order with vendor

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -48,20 +48,7 @@ namespace VendorOrderTracker.Tests
       Assert.AreEqual(1, result);
     }
 
-    // [TestMethod]
-    // public void GetAll_ReturnsAllVendorObjects_VendorList()
-    // {
-    //   string name01 = "Suzie's Cafe";
-    //   string name02 = "Coffee Research Institute";
-    //   string description01 = "cafe local ";
-    //   string description02 = "weird coffee shop";
-    //   Vendor newVendor1 = new Vendor(name01, description01);
-    //   Vendor newVendor2 = new Vendor(name02, description02);
-    //   List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
-    //   List<Vendor> result = Vendor.GetAll();
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
-
+    
     [TestMethod]
     public void GetAll_ReturnsVendors_VendorsList()
     {
@@ -91,20 +78,20 @@ namespace VendorOrderTracker.Tests
       Assert.AreEqual(newVendor2, result);
     }
 
-      // [TestMethod]
-      // public void AddOrder_AssociatesOrderWithVendor_OrderList()
-      // {
-      //   string title = "CRI Regular";
-      //   string description = "10 pastries";
-      //   int price = 17;
-      //   DateTime date = DateTime.Today;
-      //   Order newOrder= new Order(title, description, price, date);
-      //   List<Order> newList = new List<Order> { newOrder };
-      //   string name = "Coffee Research Institute";
-      //   Vendor newVendor = new Vendor(name, description);
-      //   newVendor.AddOrder(newOrder);
-      //   List<Order> result = newVendor.Orders;
-      //   CollectionAssert.AreEqual(newList, result); 
-      // }
+      [TestMethod]
+      public void AddOrder_AssociatesOrderWithVendor_OrderList()
+      {
+        string title = "CRI Regular";
+        string description = "10 pastries";
+        int price = 17;
+        DateTime date = DateTime.Today;
+        Order newOrder= new Order(title, description, price, date);
+        List<Order> newList = new List<Order> { newOrder };
+        string name = "Coffee Research Institute";
+        Vendor newVendor = new Vendor(name, description);
+        newVendor.AddOrder(newOrder);
+        List<Order> result = newVendor.Orders;
+        CollectionAssert.AreEqual(newList, result); 
+      }
   }
   }
